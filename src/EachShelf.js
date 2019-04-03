e8f50448c8da2256be21e32e1bfc6fc3bc2abbb5
Mateus Vilaverde <mateusvilaverde@gmail.com>
2019-04-03 12:45:49 -0300

    Turning EachShelf to a Functional Component

--- src/EachShelf.js
+++ src/EachShelf.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 import PropTypes from 'prop-types'
 
-class EachShelf extends Component {
-    render() {
-        const { title, which, shelfUpdater  } = this.props
-        
-        return(
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">{title}</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-              {/*
-                  Iterates through the array and renders each book 
-              */}
-                {which.map((id) => 
-                    <li key={id}>
-                        <Book shelfUpdater={shelfUpdater} id={id}/>
-                    </li>
-                )}
-              </ol>
-            </div>
-          </div>
-        )
-    }
+// Since this component doesn't use states, I've made it as a Functional Component
+const EachShelf = ({title, which, shelfUpdater}) => {
+    return(
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+          {/*
+              Iterates through the array and renders each book 
+          */}
+            {which.map((id) => 
+                <li key={id}>
+                    <Book shelfUpdater={shelfUpdater} id={id}/>
+                </li>
+            )}
+          </ol>
+        </div>
+      </div>
+    )
 }
+
 
 EachShelf.propTypes = {
   shelfUpdater: PropTypes.func.isRequired
